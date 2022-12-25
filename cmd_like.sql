065ecc952ad983a6fc89c963eb54b0d61dd3c099
yaya <yeleta2014yaya@gmail.com>
2022-12-25 17:04:29 +0000

    commande like

---
+++ cmd_like.sql
@@ -0,0 +1,5 @@
+select first_name, last_name 
+From customer
+where first_name like 't%A'
+
+; 
---
+++ cmd_order_by.sql
@@ -0,0 +1,5 @@
+select first_name, last_name 
+From customer
+order by first_name asc , last_name desc
+
+; 
