013896377f72491aa2595bbed9517a6d13915338
yaya <yeleta2014yaya@gmail.com>
2023-01-09 17:24:33 +0000

    commmandes agregation moyenne somme count

--- cmd_like.sql
+++ cmd_like.sql
@@ -1,4 +1,7 @@
-select customer_id,rental_id,return_date from rental
-where customer_id IN (1,2,3,4)
-order by return_date desc
+select staff_id ,count(*)
+ from payment
+ group by staff_id
+ order by 2 desc
+
+
 ;
